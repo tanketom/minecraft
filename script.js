a848f1eefc99c243320ba28cd1256d638e713936
tanketom <tanketom@gmail.com>
2024-09-10 17:37:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function updatePlanksSlider() {
+    let planks = document.getElementById('planks').value;
+    document.getElementById('planksSlider').value = planks;
+}
+
+function updatePlanksInput() {
+    let planksSlider = document.getElementById('planksSlider').value;
+    document.getElementById('planks').value = planksSlider;
+}
+
+function updateSlabsSlider() {
+    let slabs = document.getElementById('slabs').value;
+    document.getElementById('slabsSlider').value = slabs;
+}
+
+function updateSlabsInput() {
+    let slabsSlider = document.getElementById('slabsSlider').value;
+    document.getElementById('slabs').value = slabsSlider;
+}
+
 function calculate() {
     let planks = parseInt(document.getElementById('planks').value) || 0;
     let slabs = parseInt(document.getElementById('slabs').value) || 0;
@@ -11,5 +31,5 @@ function calculate() {
     // Calculate the number of stacks of tree blocks needed
     let stacks = Math.ceil(treeBlocks / 64);
 
-    document.getElementById('result').innerText = `You need to cut ${stacks} stacks (${treeBlocks} tree blocks) of Acacia trees to get ${planks} planks and ${slabs} slabs.`;
+    document.getElementById('result').innerText = `You need to cut ${stacks} stacks (${treeBlocks} tree blocks) to get ${planks} planks and ${slabs} slabs.`;
 }
