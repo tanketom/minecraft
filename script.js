cff16fff8a19b3dbdc007a361eed87d1beebcad0
tanketom <tanketom@gmail.com>
2024-09-10 19:46:29 +0200

    push

--- script.js
+++ script.js
@@ -31,5 +31,19 @@ function calculate() {
     // Calculate the number of stacks of tree blocks needed
     let stacks = Math.ceil(treeBlocks / 64);
 
-    document.getElementById('result').innerText = `You need to cut ${stacks} stacks (${treeBlocks} tree blocks) to get ${planks} planks and ${slabs} slabs.`;
+    // Calculate sticks (2 planks = 4 sticks)
+    let sticks = Math.floor(planks / 2) * 4;
+
+    document.getElementById('result').innerText = `You need to cut ${stacks} stacks (${treeBlocks} tree blocks) to get ${planks} planks and ${slabs} slabs. You can also craft ${sticks} sticks.`;
+
+    // Resource efficiency tips
+    let tips = "Tip: Use slabs instead of full blocks to save resources!";
+    document.getElementById('tips').innerText = tips;
+
+    // Visual representation of tree blocks
+    let visual = "";
+    for (let i = 0; i < treeBlocks; i++) {
+        visual += '<img src="gfx/tree-block.png" alt="Tree Block" class="tree-block">';
+    }
+    document.getElementById('visual').innerHTML = visual;
 }
